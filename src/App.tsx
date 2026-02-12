756327e13ff4ec3335afc558905b5d83da679011
DiegoMont <diegomonmar@hotmail.com>
2026-02-12 00:18:44 -0600

    Add title to index

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import PracticePage from "./pages/PracticePage"
 
 import songs from "./data"
 import { BrowserRouter, Route, Routes } from "react-router"
+import Portfolio from "./pages/Portfolio"
 
 const App = () => {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/"></Route>
-                <Route path="/music" element={<PracticePage song={songs[0]}/>} />
+                <Route path="/" element={ <Portfolio /> }></Route>
+                <Route path="/music" element={ <PracticePage song={songs[0]}/> } />
             </Routes>
         </BrowserRouter>
   )
