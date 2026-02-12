fe6c233f822c144e2d230c227cad67c32f7f224e
DiegoMont <diegomonmar@hotmail.com>
2026-02-12 00:58:58 -0600

    Try with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import PracticePage from "./pages/PracticePage"
 
 import songs from "./data"
-import { BrowserRouter, Route, Routes } from "react-router"
+import { HashRouter, Route, Routes } from "react-router"
 import Portfolio from "./pages/Portfolio"
 
 const App = () => {
 
     return (
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={ <Portfolio /> }></Route>
                 <Route path="/music" element={ <PracticePage song={songs[0]}/> } />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
   )
 }
 
